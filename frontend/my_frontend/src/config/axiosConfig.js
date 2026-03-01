cdd54fa72213fe3cf56d4146e567a02bfb7572f5
ptobez-creator <ptobez@gmail.com>
2026-03-02 00:17:08 +0100

    Changin axiosConfig.js config to stop pointing to defult http://localhost:5000

--- frontend/my_frontend/src/config/axiosConfig.js
+++ frontend/my_frontend/src/config/axiosConfig.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { API_URL, LOCAL_API_URL } from './runtimeConfig';
 
-axios.defaults.baseURL = API_URL;
+// In production (built app), use relative URLs which will be proxied by nginx to the backend
+// In development, use the configured API_URL
+const isDevelopment = process.env.NODE_ENV === 'development';
+axios.defaults.baseURL = isDevelopment ? API_URL : '';
 
 axios.interceptors.request.use(
   (config) => {
     if (typeof config.url === 'string' && config.url.startsWith(LOCAL_API_URL)) {
-      config.url = config.url.replace(LOCAL_API_URL, API_URL);
+      config.url = config.url.replace(LOCAL_API_URL, isDevelopment ? API_URL : '');
     }
 
     return config;
